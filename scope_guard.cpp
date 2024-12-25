2baaf280e67cec57bb8d7dcd0b457fcb2107c737
Zhuo Zhang <imzhuo@foxmail.com>
2024-12-26 01:25:37 +0800

    scope_guard(defer)

---
+++ scope_guard.cpp
@@ -0,0 +1,3 @@
+// https://zhuanlan.zhihu.com/p/35191739
+// https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Scope_Guard
+// https://www.reddit.com/r/cpp/comments/s6tzcn/golanglike_defer_operation_in_c/
